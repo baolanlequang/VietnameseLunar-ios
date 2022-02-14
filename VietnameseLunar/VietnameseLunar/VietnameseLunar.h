b106a802a30e74804beb379d36f442ec4254b002
baolan2005 <baolan2005@gmail.com>
2022-02-14 17:33:47 +0100

    init project

---
+++ VietnameseLunar/VietnameseLunar/VietnameseLunar.h
@@ -0,0 +1,18 @@
+//
+//  VietnameseLunar.h
+//  VietnameseLunar
+//
+//  Created by Lan Le on 14.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VietnameseLunar.
+FOUNDATION_EXPORT double VietnameseLunarVersionNumber;
+
+//! Project version string for VietnameseLunar.
+FOUNDATION_EXPORT const unsigned char VietnameseLunarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VietnameseLunar/PublicHeader.h>
+
+
